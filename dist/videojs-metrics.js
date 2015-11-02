7f73f7de80a4086793dd1f8c3599bd941f3cb5b4
benjipott <pott.benjamin@gmail.com>
2015-11-02 17:26:52 +0100

    update bandwith for hls

--- dist/videojs-metrics.js
+++ dist/videojs-metrics.js
@@ -1,4 +1,4 @@
-/*! videojs-metrics - v0.0.0 - 2015-10-26
+/*! videojs-metrics - v0.0.0 - 2015-11-02
 * Copyright (c) 2015 benjipott; Licensed Apache-2.0 */
 /*! videojs-metrics - v0.0.0 - 2015-10-7
  * Copyright (c) 2015 benjipott
@@ -243,8 +243,8 @@
     try {
       var metrics = player.techGet('getPlaybackStatistics');
       this.metrics_ = videojs.util.mergeOptions(this.metrics_, metrics);
-      evt.video_bitrate = this.metrics_.video.bandwidth ? (Math.round(this.metrics_.video.bandwidth / 1000)) : -1;
-      evt.audio_bitrate = this.metrics_.audio.bandwidth ? (Math.round(this.metrics_.audio.bandwidth / 1000)) : -1;
+      evt.video_bitrate = this.metrics_.video.bandwidth > 0 ? Math.max(-1, Math.round(this.metrics_.video.bandwidth / 1000)) : -1;
+      evt.audio_bitrate = this.metrics_.audio.bandwidth > 0 ? Math.max(-1, Math.round(this.metrics_.audio.bandwidth / 1000)) : -1;
       var pickedData = videojs.Metrics.pick(evt, this.getRequiredKeys(evt.type));
       this.xhr(this.options(), pickedData);
     }
